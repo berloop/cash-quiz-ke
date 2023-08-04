61a52171e74ee5234f8886daa2b751e2a6826fa4
berloop <berloop19@gmail.com>
2023-08-04 17:36:30 +0300

    Score Database System

--- app/api/triviascore/route.ts
+++ app/api/triviascore/route.ts
@@ -1,3 +1,4 @@
+import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -5,33 +6,43 @@ import { NextResponse } from "next/server";
 
 
 export async function POST(
-    req: Request
+  req: Request
 ) {
-    try {
-        //check if user is authenticated...
-        const { userId } = auth();
-        const body = await req.json();
-        const { payload } = body;
+  try {
+    //check if user is authenticated...
+    const { userId } = auth();
+    const body = await req.json();
+    const { payload } = body;
 
-        if (!userId) {
-            return new NextResponse("Unauthorized User", { status: 401 });
-        }
-
-
-        if (!payload) {
-            return new NextResponse("User Score is required", { status: 400 });
-        }
-
-         const response = payload;
-
-       
-
-        return NextResponse.json("Here is your payload", response);
-      
+    if (!userId) {
+      return new NextResponse("Unauthorized User", { status: 401 });
     }
-    catch (error) {
-        console.log("[CONVERSATION_ERROR]", error);
-        return new NextResponse("Internal Error", { status: 500 })
 
+
+    if (!payload) {
+      return new NextResponse("User Score is required", { status: 400 });
     }
+
+    const response = payload;
+
+    // Save the payload along with the user ID in the database
+    const userScoreData = await prismadb.triviaScore.create({
+      data: {
+        userId: userId as string,
+        score: payload.score,
+        showName: payload.showName,
+        timestamp: payload.timestamp,
+      },
+    });
+
+
+
+    return NextResponse.json("Here is your payload", response);
+
+  }
+  catch (error) {
+    console.log("[CONVERSATION_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 })
+
+  }
 }
