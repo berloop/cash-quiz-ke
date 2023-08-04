5e5182985bc738060fc496f4c851ec1725106ea9
berloop <berloop19@gmail.com>
2023-08-04 18:47:44 +0300

    New Database Changes

--- app/api/triviascore/route.ts
+++ app/api/triviascore/route.ts
@@ -6,43 +6,57 @@ import { NextResponse } from "next/server";
 
 
 export async function POST(
-  req: Request
+    req: Request
 ) {
-  try {
-    //check if user is authenticated...
-    const { userId } = auth();
-    const body = await req.json();
-    const { payload } = body;
+    try {
+        //check if user is authenticated...
+        const { userId } = auth();
+        const body = await req.json();
+        const { payload } = body;
 
-    if (!userId) {
-      return new NextResponse("Unauthorized User", { status: 401 });
+        if (!userId) {
+            return new NextResponse("Unauthorized User", { status: 401 });
+        }
+
+
+        if (!payload) {
+            return new NextResponse("User Score is required", { status: 400 });
+        }
+
+        const userScoreRecord = await prismadb.triviaScore.findUnique({
+            where: {
+                userId
+            }
+        });
+    
+        if (userScoreRecord) {
+            await prismadb.triviaScore.update({
+                where: { userId: userId },
+                data: { score: userScoreRecord.score + payload.score },
+            });
+    
+        } else {
+            await prismadb.triviaScore.create({
+                data: {
+                  userId: userId as string,
+                  score: payload.score,
+                  showName: payload.showName,
+                  timestamp: payload.timestamp,
+                },
+              });
+        
     }
 
+         const response = payload;
 
-    if (!payload) {
-      return new NextResponse("User Score is required", { status: 400 });
+
+
+        return NextResponse.json("Here is your payload", response);
+      
     }
+    catch (error) {
+        console.log("[CONVERSATION_ERROR]", error);
+        return new NextResponse("Internal Error", { status: 500 })
 
-    const response = payload;
-
-    // Save the payload along with the user ID in the database
-    const userScoreData = await prismadb.triviaScore.create({
-      data: {
-        userId: userId as string,
-        score: payload.score,
-        showName: payload.showName,
-        timestamp: payload.timestamp,
-      },
-    });
-
-
-
-    return NextResponse.json("Here is your payload", response);
-
-  }
-  catch (error) {
-    console.log("[CONVERSATION_ERROR]", error);
-    return new NextResponse("Internal Error", { status: 500 })
-
-  }
+    }
 }
